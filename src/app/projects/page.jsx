94ef77d6ff749a415460faf8c904dadea65a83a4
Marijan Marković <85864921+marijan10marijan@users.noreply.github.com>
2024-10-09 18:25:02 +0200

    Update page.jsx

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -10,8 +10,9 @@ const ProjectsPage = async () => {
   }
 
   return (
-    <div className="py-6">
-      <h1 className="text-3xl font-bold text-center">Projects Page</h1>
+    <div className="py-6 px-4">
+      <h1 className="text-3xl font-bold text-center mb-4">Projects Page</h1>
+      <p className="text-base sm:text-lg text-center">This page is visible only to login user!!</p>
     </div>
   );
 };
